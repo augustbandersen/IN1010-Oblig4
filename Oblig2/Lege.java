8bb4e0c660e9d1e67a4fd3b51c88079f6c2391b3
augustbandersen <101282921+augustbandersen@users.noreply.github.com>
2022-03-10 11:50:33 +0100

    Update Lege.java
    D1 og D2

--- Oblig2/Lege.java
+++ Oblig2/Lege.java
@@ -1,5 +1,7 @@
-class Lege {
+class Lege implements Comparable<Lege> {
     String legeNavn;
+    IndeksertListe<Resept> utskrevneResepter = new IndeksertListe<>();
+
     //Konstruktor
     public Lege(String legeNavn){
         this.legeNavn = legeNavn;
@@ -13,4 +15,21 @@ class Lege {
     public String toString(){
         return ("Legens navn: " + legeNavn);
     }
+    
+    @Override
+    public int compareTo(Lege lege){
+        return legeNavn.toLowerCase().compareTo(lege.hentLegeNavn().toLowerCase());
+    }
+
+    public IndeksertListe<Resept> hentUtskrevneResepter(){
+        return utskrevneResepter;
+    }
+
+    HvitResept skrHvitResept (Legemiddel legemiddel, Pasient pasient, int reit) throws UlovligUtskrift{}
+
+    MilResept skrivMilResept (Legemiddel legemiddel, Pasient pasient) throws UlovligUtskrift{}
+
+    PResept skrivPResept (Legemiddel legemiddel, Pasient pasient, int reit) throws UlovligUtskrift{}
+
+    BlaaResept skrivBlaaResept (Legemiddel legemiddel, Pasient pasient, int reit) throws UlovligUtskrift{}
 }
