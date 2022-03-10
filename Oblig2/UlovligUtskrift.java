40ec3736f81ccd959eecae43b4a559d218c78757
augustbandersen <101282921+augustbandersen@users.noreply.github.com>
2022-03-10 12:17:49 +0100

    Update UlovligUtskrift.java

--- Oblig2/UlovligUtskrift.java
+++ Oblig2/UlovligUtskrift.java
@@ -1,3 +1,5 @@
+package Oblig2;
+
 class UlovligUtskrift extends Exception {
     UlovligUtskrift (Lege l, Legemiddel lm) {
         super("Legen "+l.hentLegeNavn()+" har ikke lov til aa skrive ut "+lm.hentNavn());
