f0886a8f10c0e82ea832822fe7dfd624e6feedb4
augustbandersen <101282921+augustbandersen@users.noreply.github.com>
2022-03-10 12:59:39 +0100

    Update Lege.java

--- Sykehus/Lege.java
+++ Sykehus/Lege.java
@@ -28,11 +28,46 @@ class Lege implements Comparable<Lege> {
         return utskrevneResepter;
     }
 
-    HvitResept skrHvitResept (Legemiddel legemiddel, Pasient pasient, int reit) throws UlovligUtskrift{}
+    HvitResept skrHvitResept (Legemiddel legemiddel, Pasient pasient, int reit) throws UlovligUtskrift{
+        if (!(this instanceof Spesialist) && (legemiddel instanceof Narkotisk)){
+            throw new UlovligUtskrift(this, legemiddel);
+        }
 
-    MilResept skrivMilResept (Legemiddel legemiddel, Pasient pasient) throws UlovligUtskrift{}
+        HvitResept hvitResept = new HvitResept(legemiddel, this, pasient.hentId(), reit);
+        utskrevneResepter.leggTil(hvitResept);
+        return hvitResept;
+    }
 
-    PResept skrivPResept (Legemiddel legemiddel, Pasient pasient, int reit) throws UlovligUtskrift{}
+    MilResept skrivMilResept (Legemiddel legemiddel, Pasient pasient) throws UlovligUtskrift{
+        if (!(this instanceof Spesialist) && (legemiddel instanceof Narkotisk)){
+            throw new UlovligUtskrift(this, legemiddel);
+        }
 
-    BlaaResept skrivBlaaResept (Legemiddel legemiddel, Pasient pasient, int reit) throws UlovligUtskrift{}
+        MilResept milResept = new MilResept(legemiddel, this, pasient.hentId());
+        utskrevneResepter.leggTil(milResept);
+        return milResept;
+    }
+
+    PResept skrivPResept (Legemiddel legemiddel, Pasient pasient, int reit) throws UlovligUtskrift{  
+        if (!(this instanceof Spesialist) && (legemiddel instanceof Narkotisk)){
+            throw new UlovligUtskrift(this, legemiddel);
+        }
+
+        PResept pResept = new PResept(legemiddel, this, pasient.hentId(), reit);
+        utskrevneResepter.leggTil(pResept);
+        return pResept;
+    }
+
+    BlaaResept skrivBlaaResept (Legemiddel legemiddel, Pasient pasient, int reit) throws UlovligUtskrift{  
+        if (!(this instanceof Spesialist) && (legemiddel instanceof Narkotisk)){
+            throw new UlovligUtskrift(this, legemiddel);
+        }
+
+        //Spesialister kan alltid skrive ut Narkotiske legemidler men bare på blaa resept.
+        //(Skal fikse dette ^. Naa kan spesialist skrive ut Narkotiske legemidler paa alle resepter)
+
+        BlaaResept blaaResept = new BlaaResept(legemiddel, this, pasient.hentId(), reit);
+        utskrevneResepter.leggTil(blaaResept);
+        return blaaResept;
+    }
 }
